5e8d4603fd26132e024c2c3c5a3f2245a7e08c8c
Møffenzeef Mødular <rossandrewfish@gmail.com>
2016-05-21 21:11:19 -0700

    Update and rename GMO_Artist_Series_Bassic.ino to GMO_Artist_Series_Dogs_And_Lasers.ino

--- AlternateFirmware/AlternateSampleBanks/GMO_Artist_Series_Dogs_And_Lasers.ino
+++ AlternateFirmware/AlternateSampleBanks/GMO_Artist_Series_Dogs_And_Lasers.ino
@@ -17,6 +17,7 @@ _____/\\\\\\\\\\\\__/\\\\____________/\\\\_______/\\\\\______
     http://moffenzeefmodular.com   
   
   Mozzi Synthesis Library by Tim Barrass CC-BY-NC-SA
+   Inspired by/pieces of code from a variety of sketches included in the Mozzi library in the "Samples" example folder
 
 /////////////////////////////////////////////////////////////////
           DEPENDENCIES 
